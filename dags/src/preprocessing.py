6a4c8160a3067a8ec02b068d15d483c1b58c6d44
pingsutw <pingsutw@gmail.com>
2019-12-19 10:40:20 +0800

    refactor preprocessing.py

--- dags/src/preprocessing.py
+++ dags/src/preprocessing.py
@@ -5,7 +5,9 @@ import seaborn as sns
 color = sns.color_palette()
 sns.set_style('darkgrid')
 import sklearn
+from sklearn.preprocessing import LabelEncoder
 from scipy.stats import norm, skew
+from scipy.special import boxcox1p
 
 # Limiting floats output to 3 decimal points
 pd.set_option('display.float_format', lambda x: '{:.3f}'.format(x))
@@ -14,10 +16,6 @@ logger = logging.getLogger(__name__)
 
 
 def preprocessing(**kwargs):
-    # ti = kwargs['ti']
-    # loaded = ti.xcom_pull(task_ids='load_data')
-    # new_samples = loaded[0]
-    # test_set = loaded[1]
 
     logging.info('variables successfully fetched from previous task')
     logging.info('Start preprocessing data')
@@ -44,43 +42,31 @@ def preprocessing(**kwargs):
     all_data.drop(['SalePrice'], axis=1, inplace=True)
     logging.info("all_data size is : {}".format(all_data.shape))
 
-    all_data["PoolQC"] = all_data["PoolQC"].fillna("None")
-    all_data["MiscFeature"] = all_data["MiscFeature"].fillna("None")
-    all_data["Alley"] = all_data["Alley"].fillna("None")
-    all_data["Fence"] = all_data["Fence"].fillna("None")
-    all_data["FireplaceQu"] = all_data["FireplaceQu"].fillna("None")
-    for col in ('GarageType', 'GarageFinish', 'GarageQual', 'GarageCond'):
+    for col in ('GarageType', 'GarageFinish', 'GarageQual', 'GarageCond', 'PoolQC', 'MiscFeature', 'Alley',
+                'Fence', 'FireplaceQu', 'MasVnrType'):
         all_data[col] = all_data[col].fillna('None')
+
     for col in ('GarageYrBlt', 'GarageArea', 'GarageCars'):
         all_data[col] = all_data[col].fillna(0)
 
     all_data["LotFrontage"] = all_data.groupby("Neighborhood")["LotFrontage"].transform(
         lambda x: x.fillna(x.median()))
-    for col in ('BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF', 'BsmtFullBath', 'BsmtHalfBath'):
+
+    for col in ('BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF', 'BsmtFullBath', 'BsmtHalfBath', 'MasVnrArea'):
         all_data[col] = all_data[col].fillna(0)
-    for col in ('BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2'):
+
+    for col in ('BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2', 'MSSubClass'):
         all_data[col] = all_data[col].fillna('None')
-    all_data["MasVnrType"] = all_data["MasVnrType"].fillna("None")
-    all_data["MasVnrArea"] = all_data["MasVnrArea"].fillna(0)
-    all_data['MSZoning'] = all_data['MSZoning'].fillna(all_data['MSZoning'].mode()[0])
+
+    for col in ('MSZoning', 'Electrical', 'KitchenQual', 'Exterior1st', 'Exterior2nd', 'SaleType'):
+        all_data[col] = all_data[col].fillna(all_data[col].mode()[0])
+
+    for col in ('MSSubClass', 'OverallCond', 'YrSold', 'MoSold'):
+        all_data[col] = all_data[col].astype(str)
+
     all_data = all_data.drop(['Utilities'], axis=1)
     all_data["Functional"] = all_data["Functional"].fillna("Typ")
-    all_data['Electrical'] = all_data['Electrical'].fillna(all_data['Electrical'].mode()[0])
-    all_data['KitchenQual'] = all_data['KitchenQual'].fillna(all_data['KitchenQual'].mode()[0])
-    all_data['Exterior1st'] = all_data['Exterior1st'].fillna(all_data['Exterior1st'].mode()[0])
-    all_data['Exterior2nd'] = all_data['Exterior2nd'].fillna(all_data['Exterior2nd'].mode()[0])
-    all_data['SaleType'] = all_data['SaleType'].fillna(all_data['SaleType'].mode()[0])
-    all_data['MSSubClass'] = all_data['MSSubClass'].fillna("None")
 
-    # MSSubClass=The building class
-    all_data['MSSubClass'] = all_data['MSSubClass'].apply(str)
-    # Changing OverallCond into a categorical variable
-    all_data['OverallCond'] = all_data['OverallCond'].astype(str)
-    # Year and month sold are transformed into categorical features.
-    all_data['YrSold'] = all_data['YrSold'].astype(str)
-    all_data['MoSold'] = all_data['MoSold'].astype(str)
-
-    from sklearn.preprocessing import LabelEncoder
     cols = ('FireplaceQu', 'BsmtQual', 'BsmtCond', 'GarageQual', 'GarageCond',
             'ExterQual', 'ExterCond', 'HeatingQC', 'PoolQC', 'KitchenQual', 'BsmtFinType1',
             'BsmtFinType2', 'Functional', 'Fence', 'BsmtExposure', 'GarageFinish', 'LandSlope',
@@ -93,32 +79,29 @@ def preprocessing(**kwargs):
         all_data[c] = lbl.transform(list(all_data[c].values))
 
     # shape
-    print('Shape all_data: {}'.format(all_data.shape))
+    logging.info('Shape all_data: {}'.format(all_data.shape))
 
     all_data['TotalSF'] = all_data['TotalBsmtSF'] + all_data['1stFlrSF'] + all_data['2ndFlrSF']
-
     numeric_feats = all_data.dtypes[all_data.dtypes != "object"].index
 
     # Check the skew of all numerical features
     skewed_feats = all_data[numeric_feats].apply(lambda x: skew(x.dropna())).sort_values(ascending=False)
-    print("\nSkew in numerical features: \n")
+    logging.info("\nSkew in numerical features: \n")
     skewness = pd.DataFrame({'Skew': skewed_feats})
 
     skewness = skewness[abs(skewness) > 0.75]
-    print("There are {} skewed numerical features to Box Cox transform".format(skewness.shape[0]))
+    logging.info("There are {} skewed numerical features to Box Cox transform".format(skewness.shape[0]))
 
-    from scipy.special import boxcox1p
     skewed_features = skewness.index
     lam = 0.15
     for feat in skewed_features:
         all_data[feat] = boxcox1p(all_data[feat], lam)
 
     all_data = pd.get_dummies(all_data)
-    print(all_data.shape)
+    logging.info(all_data.shape)
 
     train = all_data[:ntrain]
     label = y_train
     test = all_data[ntrain:]
 
-    logging.info('preprocessed the data')
     return[train, label, test, test_ID]
