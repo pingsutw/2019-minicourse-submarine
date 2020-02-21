d0a1deb5a0dda452eda20930f0e3cad6f9de9f39
hsuyuming <abego452@gmail.com>
2020-02-21 08:37:04 +0800

    modify the code, make it can support two ML algorithm

--- dags/src/pusher.py
+++ dags/src/pusher.py
@@ -12,6 +12,10 @@ def save_model_local(**kwargs):
 
     ti = kwargs['ti']
     xgboost, lightgbm = ti.xcom_pull(task_ids='model_validation')
+    # xgboost = ti.xcom_pull(task_ids='model_validation')
 
     path = os.path.join(kwargs['path_model_save_dir'], 'xgboost')
     mlflow.xgboost.save_model(xgboost, path)
+
+    path = os.path.join(kwargs['path_model_save_dir'], 'lightgbm')
+    mlflow.lightgbm.save_model(lightgbm, path)
